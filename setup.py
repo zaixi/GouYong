23a41364aa1deb7ec34ead7a85dd07bd5fd6e1a5
liyj <liyj@nationalchip.com>
2018-09-29 16:24:12 +0800

    更新setup.py的long_description

--- setup.py
+++ setup.py
@@ -6,6 +6,24 @@ from setuptools import setup,find_packages
 import os
 import os.path
 import sys
+import requests
+
+def md_to_rst(from_file, to_file):
+    """
+    将markdown格式转换为rst格式
+    @param from_file: {str} markdown文件的路径
+    @param to_file: {str} rst文件的路径
+    """
+    response = requests.post(
+        url='http://c.docverter.com/convert',
+        data={'to': 'rst', 'from': 'markdown'},
+        files={'input_files[]': open(from_file, 'rb')}
+    )
+
+    if response.ok:
+        with open(to_file, "wb") as f:
+            f.write(response.content)
+
 def get_data_files():
     data_files=[]
     for path,subdirs,files in os.walk("GouYong/share"):
@@ -13,10 +31,22 @@ def get_data_files():
             data_files.append((os.path.join(sys.prefix,path[path.find('/')+1:]),[os.path.join(path,file) for file in files]))
     return data_files
 
+def get_file(*paths):
+    path = os.path.join(*paths)
+    try:
+        with open(path, 'rb') as f:
+            return f.read().decode('utf8')
+    except IOError:
+        pass
+
+def get_readme():
+    md_to_rst("README.md", "README.rst")
+    return get_file(os.path.dirname(__file__), 'README.rst')
+
 if __name__=="__main__":
     setup(
         name = "GouYong",
-        version = '0.4.4',
+        version = '0.4.6',
         packages = find_packages(),
         #include_package_data=True,
         install_requires = [
@@ -41,6 +71,7 @@ if __name__=="__main__":
         author = "Liyj",
         author_email = "liyujiangwork@gmail.com",
         description = "A translation software on  linux",
+        long_description = get_readme(),
         license = "GPL",
         url = "https://github.com/zaixi/GouYong",
         classifiers=[
